682ad9e9004c9778733144667981842ff17b34db
greg <gregjsmith@gmx.com>
2023-06-16 02:28:27 +0300

    remove cloud block

--- backend.tf
+++ backend.tf
@@ -4,13 +4,14 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
+/*
   cloud {
-    organization = "{{ORGANIZATION_NAME}}"
+    organization = "example-org-761f9f"
 
     workspaces {
-      name = "{{WORKSPACE_NAME}}"
+      name = "getting-started"
     }
   }
-
+*/
   required_version = ">= 1.1.2"
 }
